a5ddf0da4fe3a94138e597c61d3d97c54aad4fe2
jasonucsi <21502@ucsigroup.com.my>
2024-09-19 11:22:06 +0800

    link

---
+++ arrival/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/ucsi_logo_footer.png">
+    <LandingComponent/>
+  </div>
+</template>
+
+<script>
+import LandingComponent from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ arrival/src/components/Landing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="pre-arrival">
+      <h1>International Pre-Arrival Checklist</h1>
+      <a href="https://www.ucsiuniversity.edu.my/Admission-Pre-Arrival/international/kuala-lumpur-campus" target="_blank">Click Here!</a>
+    </div>
+    <div class="arrival">
+      <h1>International Arrival Checklist</h1>
+      <a href="https://www.ucsiuniversity.edu.my/admission-arrival/international/Kuala-lumpur-campus" target="_blank">Click Here!</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingComponent'
+}
+</script>
+
+<style scoped>
+.pre-arrival {
+  margin-bottom: 50px; /* Adjust this value to control the space */
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
